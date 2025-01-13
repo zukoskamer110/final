ca2fa32edc3fe935829466e1b0b585990d9ab846
Abdulboriy <ggandabdubori@gmail.com>
2025-01-13 19:13:55 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter,Routes,Route} from react-router
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes 
+      <Route path="/" element></Route>
+    </Routes >
+  </BrowserRouter>,
+)
